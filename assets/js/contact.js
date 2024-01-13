d275d2181cf0021b0f0c48309a473d655d5c825d
Zahra Hamdane <hamdane.zahra@gmail.com>
2024-01-13 17:04:23 +0100

    Section & Page Contact

---
+++ assets/js/contact.js
@@ -0,0 +1,36 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_n5cpl2s",
+      "template_4kkfh5e",
+      "#contact-form",
+      "cdKPOS2WF-_ZUGdF9"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
